aa6b20123088153c4fdbd7618a8a440f813ac604
Dpac07 <deepakbabu22803@gmail.com>
2021-10-23 09:58:27 +0530

    new

---
+++ src/org/git/Git.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class Git {
+	public void empid() {
+		System.out.println("emp id is 123");
+			}
+private void empName() {
+System.out.println("emp name is xyz");
+
+}
+public static void main(String[] args) {
+	Git g=new Git();
+	g.empid();
+	g.empName();
+}
+}
