f89e93a892d7200bc43073d49e2d1ddb68daade1
Dpac07 <deepakbabu22803@gmail.com>
2021-10-23 10:08:12 +0530

    update

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -8,6 +8,9 @@ private void empName() {
 System.out.println("emp name is xyz");
 
 }
+private void empNumber() {
+	System.out.println("employee number is");
+}
 public static void main(String[] args) {
 	Git g=new Git();
 	g.empid();
